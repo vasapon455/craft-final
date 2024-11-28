0eab7a1a5fb39c6da39e3b8ee318bbda19a8964f
Vasapon <vasapon1996@gmail.com>
2024-11-28 14:00:20 +0700

    change

--- final_project/frontend/src/components/NavBar.jsx
+++ final_project/frontend/src/components/NavBar.jsx
@@ -8,9 +8,10 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
   const [clickHamburger, setClickHamburger] = useState(false);
 
-  const toggleHamburger = () => {
-    setClickHamburger(!seClickHamburger);
-  };
+  function toggleHamburger(){
+    setClickHamburger((value)=>!value)
+  }
+
 
   return (
     <Navbar className="navbar-container" expand="xl">
@@ -21,68 +22,136 @@ const NavBar = () => {
               <img src="/navbar/user.svg" className="navbar-login" />
             </Link>
           </Col>
-          <Col md={4} lg={3} style={{zIndex: "4"}} >
+          <Col md={4} lg={3} style={{ zIndex: "4" }}>
             <img
               className="navbar-logo"
               src={
                 clickHamburger ? "navbar/logo-click-ham.svg" : "navbar/logo.svg"
               }
-              
             />
           </Col>
           <Col
             lg={12}
             className={clickHamburger ? "hamburger-menu" : "navbar-menu "}
           >
-            <ul className={clickHamburger? "hamburger-menu-inner":"navbar-menu-container"}>
+            <ul
+              className={
+                clickHamburger
+                  ? "hamburger-menu-inner"
+                  : "navbar-menu-container"
+              }
+            >
               <li>
-                <Link to="/" className= {clickHamburger? "sub-heading black navbar-menu-text" : "paragraph white navbar-menu-text"}  >
+                <Link
+                  to="/"
+                  className={
+                    clickHamburger
+                      ? "sub-heading black navbar-menu-text"
+                      : "paragraph white navbar-menu-text"
+                  }
+                >
                   Home
                 </Link>
               </li>
               <li>
-                <Link to="/article" className={clickHamburger? "sub-heading black navbar-menu-text" : "paragraph white navbar-menu-text"}>
+                <Link
+                  to="/article"
+                  className={
+                    clickHamburger
+                      ? "sub-heading black navbar-menu-text"
+                      : "paragraph white navbar-menu-text"
+                  }
+                >
                   Article
                 </Link>
               </li>
 
               <li>
-                <Link to="/shopping" className={clickHamburger? "sub-heading black navbar-menu-text" : "paragraph white navbar-menu-text"}>
+                <Link
+                  to="/shopping"
+                  className={
+                    clickHamburger
+                      ? "sub-heading black navbar-menu-text"
+                      : "paragraph white navbar-menu-text"
+                  }
+                >
                   Shopping
                 </Link>
               </li>
 
               <li>
-                <Link to="/order-tracking" className={clickHamburger? "sub-heading black navbar-menu-text" : "paragraph white navbar-menu-text"}>
+                <Link
+                  to="/order-tracking"
+                  className={
+                    clickHamburger
+                      ? "sub-heading black navbar-menu-text"
+                      : "paragraph white navbar-menu-text"
+                  }
+                >
                   Order Tracking
                 </Link>
               </li>
 
               <li>
-                <Link to="/contact-us" className={clickHamburger? "sub-heading black navbar-menu-text" : "paragraph white navbar-menu-text"}>
+                <Link
+                  to="/contact-us"
+                  className={
+                    clickHamburger
+                      ? "sub-heading black navbar-menu-text"
+                      : "paragraph white navbar-menu-text"
+                  }
+                >
                   Contact Us
                 </Link>
               </li>
 
               <li>
-                <Link to="/cart" className={clickHamburger? "sub-heading black navbar-menu-text" : "paragraph white navbar-menu-text"}>
+                <Link
+                  to="/cart"
+                  className={
+                    clickHamburger
+                      ? "sub-heading black navbar-menu-text"
+                      : "paragraph white navbar-menu-text"
+                  }
+                >
                   Cart
                 </Link>
               </li>
 
               <li>
-                <Link to="/login" className={clickHamburger? "sub-heading black navbar-menu-text" : "paragraph white navbar-menu-text"}>
+                <Link
+                  to="/login"
+                  className={
+                    clickHamburger
+                      ? "sub-heading black navbar-menu-text"
+                      : "paragraph white navbar-menu-text"
+                  }
+                >
                   Login/Register
                 </Link>
               </li>
             </ul>
           </Col>
-          <Col md={4} lg={1} className="hamburger" onClick={setClickHamburger} style={{zIndex: "4"}}>
-            <div className="hamburger-container">
-              <div className="hamburger-part"></div>
-              <div className="hamburger-part"></div>
-              <div className="hamburger-part"></div>
-            </div>
+          <Col
+            md={4}
+            lg={1}
+            className="hamburger"
+            onClick={toggleHamburger}
+            style={{ zIndex: "4" }}
+          >
+            {!clickHamburger ? (
+              <div className="hamburger-container">
+                <div className="bar1"></div>
+                <div className="bar2"></div>
+                <div className="bar3"></div>
+              </div>
+            ) : (
+              <div className="hamburger-container"  >
+                <div className="bar1 change"></div>
+                <div className="bar2 change"></div>
+                <div className="bar3 change"></div>
+              </div>
+            )}
           </Col>
         </Row>
       </Container>
