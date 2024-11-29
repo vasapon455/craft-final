7be71661e63c21a4d141e24e5cb3350f9e9ce801
Vasapon <vasapon1996@gmail.com>
2024-11-29 09:10:52 +0700

    change

--- final_project/backend/backend/urls.py
+++ final_project/backend/backend/urls.py
@@ -21,15 +21,9 @@ from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-<<<<<<< HEAD
-    path("",include("api.urls")),
-    path("admin/", admin.site.urls),
-=======
     path("api/", include("api.urls")),
->>>>>>> 65499680213b36f150acc956f0acb80d0d54584e
     path("api/user/register/", CreateUserView.as_view(), name="register"),
     path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("api-auth/", include("rest_framework.urls")),
-    
 ]
