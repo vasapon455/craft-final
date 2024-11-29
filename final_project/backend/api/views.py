376eafa2826b1d7b736810e3ba0c93c3e975cf57
Vasapon <vasapon1996@gmail.com>
2024-11-29 10:10:36 +0700

    change

--- final_project/backend/api/views.py
+++ final_project/backend/api/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render,reverse
 from django.contrib.auth.models import User
-from .models import Article
+from .models import Article,ShopItem,CartItem,SalesOrder
 from rest_framework import generics
-from .serializers import UserSerializer,ArticleSerializer
+from .serializers import UserSerializer,ArticleSerializer,ShopItemSerializer,CartItemSerializer,SalesOrderSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
 
@@ -16,7 +16,17 @@ class ArticleData (generics.ListAPIView):
     serializer_class = ArticleSerializer
     permission_classes = [AllowAny]
 
-class ShopItemData (generics.ListAPIVIew):
-    queryset = Article.objects.all()
+class ShopItemData (generics.ListAPIView):
+    queryset = ShopItem.objects.all()
     serializer_class = ShopItemSerializer
+    permission_classes = [AllowAny]
+
+class CartItemData (generics.ListAPIView):
+    queryset = CartItem.objects.all()
+    serializer_class = CartItemSerializer
+    permission_classes = [AllowAny]
+
+class SalesOrderData (generics.ListAPIView):
+    queryset = SalesOrder.objects.all()
+    serializer_class = SalesOrderSerializer
     permission_classes = [AllowAny]
