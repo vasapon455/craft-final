c1235e72abfc094ad3b77967a41d356d6f2713f6
Vasapon <vasapon1996@gmail.com>
2024-12-02 15:00:19 +0700

    change

--- final_project/frontend/src/pages/ForgotPassword.jsx
+++ final_project/frontend/src/pages/ForgotPassword.jsx
@@ -3,39 +3,52 @@ import Layout from "../components/Layout";
 import Section from "../components/Section";
 import { Form, Button, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import LoadingIndicator from "../components/LoadingIndicator"
 
 const ForgotPassword = () => {
-
-  const [userData,setUserdata] = useState()
-
-  const getUser = ()=>{
-    api.get("/api/shop-items/", { withCredentials: true })
-    .then((res) => setProductData(res.data))
-    .catch((error) => {
-      console.error("Error fetching the comments.", error);
-    });
-  }
+  const [userEmail,setUserEmail] = useState("")
+  const [userPhone, setUserPhone] = useState("")
+  const [loading, setLoading] = useState(false);
 
 
   return (
     <Layout>
       <Section header="Forgot Password">
-        <Form className="form-container">
-        <Form.Group controlId="formName" className="mb-3 form-group">
-        <Form.Label className="paragraph black">Email Address</Form.Label>
-            <Form.Control
-              type="email"
-              placeholder="กรอกชื่อผู้ใช้งาน"
-              className="field"
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </Form.Group>
-        </Form>
-        <Container className="button-group">
-          <Button variant="red" type="submit" className="primary-button">
-          <p className="paragraph white">ขอรหัสผู้ใช้</p>
-          </Button>
-        </Container>
+        {!loading ? (
+          <Form className="form-container" >
+            <Form.Group controlId="username" className="form-group">
+              <Form.Label className="paragraph black">Email Address</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="กรอกที่อยู่อีเมล"
+                className="field"
+                onChange={(e) => setUserEmail(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3 form-group" controlId="password">
+              <Form.Label className="paragraph black">Phone Number</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="กรอกเบอร์โทรศัพท์"
+                className="field"
+                onChange={(e) => setUserPhone(e.target.value)}
+              />
+              <Link to="/forgot-password">
+                <div style={{ textAlign: "right", margin: "5px" }}>
+                </div>
+              </Link>
+            </Form.Group>
+            <Container className="button-group">
+              <Button variant="red" type="submit" className="primary-button">
+                <p className="paragraph white">ยืนยัน</p>
+              </Button>
+              <Link to="/register">
+              </Link>
+            </Container>
+          </Form>
+        ) : (
+          <LoadingIndicator />
+        )}
       </Section>
     </Layout>
   );
--- final_project/frontend/src/pages/Login.jsx
+++ final_project/frontend/src/pages/Login.jsx
@@ -4,20 +4,20 @@ import { Form, Button, Container } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import api from "../api";
+import LoadingIndicator from "../components/LoadingIndicator"
 
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const navigate = useNavigate();
-  
-   
-const handleSubmit = async (e) => {
+
+  const handleSubmit = async (e) => {
     setLoading(true);
     e.preventDefault();
     try {
-    const res = await api.post("/api/token/", { username, password });
+      const res = await api.post("/api/token/", { username, password });
       localStorage.setItem(ACCESS_TOKEN, res.data.access);
       localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
       navigate("/");
@@ -25,7 +25,7 @@ const handleSubmit = async (e) => {
       alert(error);
     } finally {
       setLoading(false);
-      location.reload()
+      location.reload();
     }
   };
 
@@ -33,49 +33,47 @@ const handleSubmit = async (e) => {
     <Layout>
       <Section header="Login">
         {!loading ? (
-            <Form className="form-container" onSubmit={handleSubmit}>
-          <Form.Group controlId="username" className="form-group">
-            <Form.Label className="paragraph black">Username</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="กรอกชื่อผู้ใช้งาน"
-              className="field"
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3 form-group" controlId="password">
-            <Form.Label className="paragraph black">Password</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="กรอกรหัสผ่าน"
-              className="field"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-           <Link to="/forgot-password">
-            <div style={{ textAlign: "right", margin: "5px" }}>
-            <span className="caption black underline">
-                      Forgot Password?
-                    </span>
-            </div>
-          </Link>
-          </Form.Group>
-                <Container className="button-group">
-          <Button variant="red" type="submit" className="primary-button">
-            <p className="paragraph white">Login</p>
-          </Button>
-          <Link to="/register">
-            <p className="paragraph black underline">Register</p>
-          </Link>
-        </Container>
-</Form>
-          ) : (
-            <LoadingIndicator />
-          )}
+          <Form className="form-container" onSubmit={handleSubmit}>
+            <Form.Group controlId="username" className="form-group">
+              <Form.Label className="paragraph black">Username</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="กรอกชื่อผู้ใช้งาน"
+                className="field"
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3 form-group" controlId="password">
+              <Form.Label className="paragraph black">Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="กรอกรหัสผ่าน"
+                className="field"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <Link to="/forgot-password">
+                <div style={{ textAlign: "right", margin: "5px" }}>
+                  <span className="caption black underline">
+                    Forgot Password?
+                  </span>
+                </div>
+              </Link>
+            </Form.Group>
+            <Container className="button-group">
+              <Button variant="red" type="submit" className="primary-button">
+                <p className="paragraph white">Login</p>
+              </Button>
+              <Link to="/register">
+                <p className="paragraph black underline">Register</p>
+              </Link>
+            </Container>
+          </Form>
+        ) : (
+          <LoadingIndicator />
+        )}
       </Section>
     </Layout>
   );
 };
-
-
 
 export default Login;
