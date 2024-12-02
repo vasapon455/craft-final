b2501d7098debcff7e609b6ca1031418af17b035
Vasapon <vasapon1996@gmail.com>
2024-12-02 09:41:37 +0700

    change

--- final_project/frontend/src/components/Footer.jsx
+++ final_project/frontend/src/components/Footer.jsx
@@ -1,10 +1,19 @@
 import "../styles/footer.css";
 import Container from "react-bootstrap/Container";
-
+import { Link } from "react-router-dom";
 import {Row, Col} from "react-bootstrap";
 
 
 const Footer = () => {
+
+    const facebookLink = "https://www.facebook.com"
+    const InstagramLink = "https://www.instagram.com/"
+    const xLink = "https://x.com/?lang=th"
+    const tikTokLink = "https://www.tiktok.com/explore"
+    const year = new Date()
+    const thisYear = year.getFullYear()
+
+
     return (
         <footer>
             <Container fluid className="footer-container">
@@ -13,10 +22,18 @@ const Footer = () => {
                 <p className="sub-heading red">Love Japanese Fashion</p>
                 <span className="paragraph white">Follow Us!</span>
                 <div className="footer-social-media">
+                    <Link to= {facebookLink}>
                     <img src="/footer/fb-logo.svg"/>
+                    </Link>
+                    <Link to ={InstagramLink}>
                     <img src="/footer/ig-logo.svg"/>
+                    </Link>
+                    <Link to={tikTokLink}>
                     <img src="/footer/tiktok-logo.svg"/>
+                    </Link>
+                    <Link to={xLink}>
                     <img src="/footer/x-logo.svg"/>
+                    </Link>
                 </div>
                 </Col> 
                 <Col Col lg={6} className="footer-right">
@@ -27,7 +44,7 @@ const Footer = () => {
             </Row>
             <Row style={{textAlign: 'center', display:'flex', flexDirection:'column', gap:'20px'}}>
                 <a className="paragraph white underline" href="#top">Return to Top</a>
-                <p className="paragraph white">Copyright © 2024 Japan Style Alrights Reserved.</p>
+                <p className="paragraph white">Copyright © {thisYear} Japan Style Alrights Reserved.</p>
             </Row>
             </Container>
         </footer>
