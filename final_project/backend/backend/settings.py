1eb6ab3bf77d59fb341f54416d353313c4d7ace8
Vasapon <vasapon1996@gmail.com>
2024-12-02 13:46:26 +0700

    change

--- final_project/backend/backend/settings.py
+++ final_project/backend/backend/settings.py
@@ -127,6 +127,18 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PWD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+    }
+}
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
