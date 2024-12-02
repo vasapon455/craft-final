f5b3ef86a73449bd6d39071f2db36d7c51de1834
Vasapon <vasapon1996@gmail.com>
2024-12-02 09:45:38 +0700

    change

--- final_project/frontend/src/pages/Login.jsx
+++ final_project/frontend/src/pages/Login.jsx
@@ -1,26 +1,24 @@
-import { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import api from "../api";
-import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
 import Layout from "../components/Layout";
 import Section from "../components/Section";
 import { Form, Button, Container } from "react-bootstrap";
-import LoadingIndicator from "../components/LoadingIndicator";
-import { useUser } from "../contexts/UserProvider";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import api from "../api";
 
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [loading, setLoading] = useState(false);
+const [loading, setLoading] = useState(false);
 
   const navigate = useNavigate();
-
-  const handleSubmit = async (e) => {
+  const submit = async (e) => {
+   
+const handleSubmit = async (e) => {
     setLoading(true);
     e.preventDefault();
     
     try {
-      const res = await api.post("/api/token/", { username, password });
+    const res = await api.post("/api/token/", { username, password });
       localStorage.setItem(ACCESS_TOKEN, res.data.access);
       localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
       navigate("/");
@@ -32,52 +30,52 @@ const Login = () => {
     }
   };
 
-    return (
-      <Layout>
-        <Section header="Login">
-          {!loading ? (
+  return (
+    <Layout>
+      <Section header="Login">
+        {!loading ? (
             <Form className="form-container" onSubmit={handleSubmit}>
-              <Form.Group controlId="username" className="form-group">
-                <Form.Label className="paragraph black">Username</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="กรอกชื่อผู้ใช้งาน"
-                  className="field"
-                  onChange={(e) => setUsername(e.target.value)}
-                />
-              </Form.Group>
-              <Form.Group className="mb-3 form-group" controlId="password">
-                <Form.Label className="paragraph black">Password</Form.Label>
-                <Form.Control
-                  type="password"
-                  placeholder="กรอกรหัสผ่าน"
-                  className="field"
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-                <Link to="/forgot-password">
-                  <div style={{ textAlign: "right", margin: "5px" }}>
-                    <span className="caption black underline">
+          <Form.Group controlId="username" className="form-group">
+            <Form.Label className="paragraph black">Username</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="กรอกชื่อผู้ใช้งาน"
+              className="field"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3 form-group" controlId="password">
+            <Form.Label className="paragraph black">Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="กรอกรหัสผ่าน"
+              className="field"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+           <Link to="/forgot-password">
+            <div style={{ textAlign: "right", margin: "5px" }}>
+            <span className="caption black underline">
                       Forgot Password?
                     </span>
-                  </div>
-                </Link>
-              </Form.Group>
-              <Container className="button-group">
-                <Button variant="red" type="submit" className="primary-button">
-                  <p className="paragraph white">Login</p>
-                </Button>
-                <Link to="/register">
-                  <p className="paragraph black underline">Register</p>
-                </Link>
-              </Container>
-            </Form>
+            </div>
+          </Link>
+          </Form.Group>
+                <Container className="button-group">
+          <Button variant="red" type="submit" className="primary-button">
+            <p className="paragraph white">Login</p>
+          </Button>
+          <Link to="/register">
+            <p className="paragraph black underline">Register</p>
+          </Link>
+        </Container>
+</Form>
           ) : (
             <LoadingIndicator />
           )}
-        </Section>
-      </Layout>
-    );
-  };
+      </Section>
+    </Layout>
+  );
+};
 
 
 export default Login;
