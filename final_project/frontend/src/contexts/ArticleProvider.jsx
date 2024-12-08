6656327717d64c15233c79e8be16a57a0d19e3f8
vasapon <vasapon1996@gmail.com>
2024-12-08 19:13:44 +0700

    change

--- final_project/frontend/src/contexts/ArticleProvider.jsx
+++ final_project/frontend/src/contexts/ArticleProvider.jsx
@@ -3,33 +3,42 @@ import api from "../api";
 
 const ArticleContext = createContext([]);
 
-const ArticleProvider = ({children}) => {
-    const [articleData,setArticleData] = useState([]);
-    const [commentData,setCommentData] = useState([])
-    
-    const getAricle = ()=>{
-      api.get("/api/article/", { withCredentials: true })
+const ArticleProvider = ({ children }) => {
+  const [articleData, setArticleData] = useState([]);
+  const [commentData, setCommentData] = useState([]);
+
+  const getAricle = () => {
+    api
+      .get("/api/article/", { withCredentials: true })
       .then((res) => setArticleData(res.data))
       .catch((error) => {
         console.error("Error fetching the articles.", error);
       });
-    }
+  };
 
-    const getComment = ()=>{
-      api.get("/api/article/comment/", { withCredentials: true })
+  const getComment = () => {
+    api
+      .get("/api/article/comment/", { withCredentials: true })
       .then((res) => setCommentData(res.data))
       .catch((error) => {
         console.error("Error fetching the comments.", error);
       });
-    }
+  };
 
-    useEffect(() => {getComment()},[commentData]);
+  useEffect(() => {
+    getComment();
+  }, []);
 
-    useEffect(()=> {getAricle()}
-    )
-   
-   return  <ArticleContext.Provider value={[articleData,commentData]}>{children}</ArticleContext.Provider>;
-  }
+  useEffect(() => {
+    getAricle();
+  }, []);
+
+  return (
+    <ArticleContext.Provider value={[articleData, commentData]}>
+      {children}
+    </ArticleContext.Provider>
+  );
+};
 
 export default ArticleProvider;
 
--- final_project/frontend/src/pages/Article.jsx
+++ final_project/frontend/src/pages/Article.jsx
@@ -8,41 +8,45 @@ import NotFound from "../components/NotFound";
 import { Container } from "react-bootstrap";
 
 const Article = () => {
-  const [data , comment] = useArticles();
-  let number = 1
+  const [data, comment] = useArticles();
+  let number = 1;
 
   return (
     <Layout>
       {data ? (
         <Section header="Article">
           <section id="article">
-            <Container style={{
-              display: "flex",
-              flexDirection: "row",
-              gap: "20px",
-              justifyContent: "center",
-              alignContent: "center",
-            }}>
-            <Link
-              to="/article/new"
+            <Container
               style={{
-                textDecoration: "underline",
+                display: "flex",
+                flexDirection: "row",
+                gap: "20px",
+                justifyContent: "center",
+                alignContent: "center",
               }}
-              className="paragraph black"
             >
-              +โพสใหม่
-            </Link>
+              <Link
+                to="/article/new"
+                style={{
+                  textDecoration: "underline",
+                }}
+                className="paragraph black"
+              >
+                +โพสใหม่
+              </Link>
             </Container>
             {data.length > 0 ? (
               data.map((data) => {
-                return <ArticleCard
-                  id={data.id}
-                  number={number++}
-                  image={data.image}
-                  header={data.title}
-                  createdDate={data.created_at}
-                  summary={data.content}
-                />;
+                return (
+                  <ArticleCard
+                    id={data.id}
+                    number={number++}
+                    image={data.image}
+                    header={data.title}
+                    createdDate={data.created_at}
+                    summary={data.content}
+                  />
+                );
               })
             ) : (
               <NotFound />
